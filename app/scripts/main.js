12a276efca278f464af08dd66772be95a7745f4f
Darren Dunne <darrenwdunne@gmail.com>
2016-08-03 11:04:17 -0400

    show / hide table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  console.log('Ready');
+  $('#inputSearch').focus();
+  // $('#searchResultsDiv').css({
+  //   display: "none",
+  //   visibility: "hidden"
+  // });
+  showSearchResults(false);
 });
 
 var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content'
@@ -12,54 +17,49 @@ var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Pag
 $(document).keypress(function(e) {
   if (e.which == 13) {
     // enter pressed
-    console.log("Enter pressed");
+    console.log('Enter pressed');
   }
 });
 
 // .on is the definitive solution to handle keypress and paste in an input field
 $('#inputSearch').on('input', function(data) {
-  console.log('Handler for .keypress() called:' + data);
   var txt = $('#inputSearch').val();
-  queryWiki(txt);
-  console.log(txt);
-  //  debugger;
+  if (txt === "") {
+    showSearchResults(false);
+  } else {
+    queryWiki(txt);
+  }
 });
 
 function queryWiki(searchString) {
   $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=' + searchString + '&callback=?', function(data) {
-//    parseSearchResults(data);
-    $('#searchResultsDiv').empty();
-    makeTable($("#searchResultsDiv"),data);
+    //    parseSearchResults(data);
+    $('#searchResultsTableHolder').empty();
+    makeTable($('#searchResultsTableHolder'), data);
   });
-}
-
-function parseSearchResults(data) {
-  //$('p').html(JSON.stringify(data));
-//  var table = $('<table></table>').addClass('foo');
-  $('#searchResultsTable tbody > tr').remove();
-  $.each(data.query.pages, function(k, v) {
-    var txt=k + ':' + v.title + ':' + v.pageid;
-
-    $('#searchResultsTable').append('<tr><td>'+v.title+'</td><td>'+v.pageid+'</td></tr>');
-
-    // var rowText='<td>'+v.title+'</td>'+'<td>'+v.pageid+'</td>';
-    //   var row = $('<tr></tr>').addClass('bar').text(rowText);
-    //   table.append(row);
-
-
-    // format for url via pageid: https://en.wikipedia.org/?curid=345430
-  });
-//  $('#searchResults').append(table);
 }
 
 function makeTable(container, data) {
-    var table = $("<table/>").addClass('table table-striped table-bordered');
-    $.each(data.query.pages, function(k, v) {
-        var row = $("<tr/>");
-        row.append("<td>"+v.title+"</td><td>"+v.pageid+"</td>");
-        table.append(row);
-    });
-    return container.append(table);
+  var table = $('<table/>').addClass('table table-striped table-bordered table-condensed');
+  //  table.addClass('table table-striped table-bordered');
+  table.append('<thead><tr><th>Page Title</th><th>ID</th></tr></thead><tbody>');
+
+  $.each(data.query.pages, function(k, v) {
+    var row = $('<tr/>');
+    row.append('<td>' + v.title + '</td><td>' + v.pageid + '</td>');
+    table.append(row);
+  });
+  table.append('</tbody>');
+  showSearchResults(true);
+  return container.append(table);
+}
+
+function showSearchResults(show) {
+  if (show) {
+    $('#searchResultsDiv').show();
+  } else {
+    $('#searchResultsDiv').hide();
+  }
 }
 
 function onError(json) {
